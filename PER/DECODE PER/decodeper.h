2f028fdb5fdd362d35e0fc7ba3140108722c7483
samaakhalidmo <101810565+samaakhalidmo@users.noreply.github.com>
2023-03-03 19:36:01 +0200

    Add files via upload

---
+++ PER/DECODE PER/decodeper.h
@@ -0,0 +1,23 @@
+#ifndef DECODERPER_H_
+#define DECODERPER_H_
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int decode(string msdencoded,char start,char len);
+string automaticActivation(string eCallMessage);
+string testCall(string eCallMessage);
+string positionCanBeTrusted(string eCallMessage);
+void structure(string eCallMessage);
+string compressedNaturalGas(string eCallMessage);
+string vehicleType(string eCallMessage);
+string dieselTankPresent(string eCallMessage);
+string electricEnergyStorage(string eCallMessage);
+string gasolineTankPresent(string eCallMessage);
+
+
+#endif
---
+++ PER/DECODE PER/main.cpp
@@ -0,0 +1,15 @@
+#include "decodeper.h"
+
+
+
+int main()
+{
+
+    structure("1D01D01440000416D0008C5BA32300");
+
+	return 0;
+}
+
+
+
+

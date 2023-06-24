cfba844b485d8bdd7aa413af69b6b3561193a5eb
Ahmed Sherif <ahmedselfiky11@gmail.com>
2023-06-24 19:31:58 +0300

    update diagrams

--- Demo/TCU/ADR-Process/src/main.h
+++ Demo/TCU/ADR-Process/src/main.h
@@ -41,7 +41,7 @@ constexpr double earthRadius = 6371000.0; // Earth's radius in meters
 #define KMH_TO_MS(speed) 								(speed*1000.0/3600.0)
 #define MILLISECONDS_TO_SECONDS(inputMilliseconds)		(inputMilliseconds / 1000)
 
-#define DATA_DIR "/home/ahmed/Desktop/Graduation-Project/Demo/Simulation-Control/Sensors-Data-Process/Actual Data/Full-Track/"
+#define DATA_DIR "/home/pi/Desktop/Graduation-Project/Demo/Simulation-Control/Sensors-Data-Process/Actual Data/Full-Track/"
 
 
 
